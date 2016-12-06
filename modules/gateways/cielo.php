2f7b33eefd7b1a7abe9437f1e0388b1209fa22f6
Weverton Velludo <wv@brasilnetwork.com.br>
2016-12-06 10:25:44 -0200

    v1.0

--- modules/gateways/cielo.php
+++ modules/gateways/cielo.php
@@ -1,4 +1,19 @@
 <?php
+/**
+ * Cielo
+ *
+ * Sistema para processamento de pagamentos cielo - BuyPage Cielo.
+ *
+ * Release inicial, com certeza pode ser melhorado
+ *
+ * @package    cielo
+ * @author     Weverton Velludo <wv@brasilnetwork.com.br>
+ * @copyright  Copyright (c) Weverton Velludo 2015
+ * @license    http://www.brasilnetwork.com.br
+ * @version    $Id$
+ * @link       http://www.brasilnetwork.com.br
+ */
+
 function cielo_MetaData() {
     return array(
         'DisplayName' => 'Cielo - Buypage Cielo',
